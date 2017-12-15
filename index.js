890b6a4f556a8ef39a31a246e5c20a0b7f00d5c6
Carey Hinoki <hinokich@amazon.com>
2017-12-15 15:22:11 -0800

    feat(forceSsl): Add `force-ssl-heroku`.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const forceSsl = require('force-ssl-heroku');
 const request = require('request');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(forceSsl)
 
 app.get('/:channel', (req, res) => {
   const url = `http://prem1.di.fm:80/${req.params.channel}?${req.query.code}`;
